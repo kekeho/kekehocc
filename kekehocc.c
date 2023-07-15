f97d46355398ef75780fab47670988b92b10e0e2
Hiroki TAKEMURA <kekeho@sfc.wide.ad.jp>
2023-07-16 04:00:28 +0900

    create: add, sub

--- kekehocc.c
+++ kekehocc.c
@@ -3,15 +3,36 @@
 
 int main(int argc, char **argv) {
     if (argc != 2) {
-        fprintf(stderr, "引数の個数が正しくありません\n");
+        fprintf(stderr, "Incorrect number of arguments\n");
         return 1;
     }
+
+    char *p = argv[1];
 
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
-    printf("\tmov rax, %d\n", atoi(argv[1]));
-    printf("\tret\n");
 
+    // copy number to mov
+    printf("\tmov rax, %ld\n", strtol(p, &p, 10));
+
+    while (*p) {
+        if (*p == '+') {
+            p++;
+            printf("\tadd rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        if (*p == '-') {
+            p++;
+            printf("\tsub rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        fprintf(stderr, "Invalid character: '%c'\n", *p);
+        return 1;
+    }
+
+    printf("\tret\n");
     return 0;
 }

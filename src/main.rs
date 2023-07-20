73a8e16b8cb9fdee88e5d95e8568e63098803020
Hiroki TAKEMURA <kekeho@sfc.wide.ad.jp>
2023-07-20 15:46:49 +0900

    add: basic arithmetic operations

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,21 @@
 use std::env;
+use std::rc::Rc;
+use std::collections::VecDeque;
 
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Debug)]
 enum TokenKind {
     Reserved,  // Symbol
     Num,  // Number
-    EOF,
 }
 
 
+#[derive(PartialEq, Debug)]
 struct Token {
     kind: TokenKind,
     val: Option<i64>,
     string: String,
 }
-
 
 impl Token {
     pub fn new(kind: TokenKind, val: Option<i64>, s: String) -> Token {
@@ -24,19 +25,68 @@ impl Token {
             string: s,
         };
     }
+}
 
-    pub fn expect_number(&self) -> i64 {
-        match self.kind {
-            TokenKind::Num => {
-                return self.val.unwrap();
+
+#[derive(PartialEq, Debug)]
+enum NodeKind {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Num {value: i64},
+}
+
+#[derive(PartialEq, Debug)]
+struct Node {
+    kind: NodeKind,
+    lhs: Option<Rc<Node>>,
+    rhs: Option<Rc<Node>>,
+}
+
+
+impl Node {
+    pub fn gen(n: &Rc<Node>) {
+        match n.kind {
+            NodeKind::Num {value: v} => {
+                println!("\tpush {}", v);
+                return
             }
 
-            _ => {
-                panic!("Expected number");
-            }
+            _ => {}
         }
+
+        Self::gen(&n.lhs.clone().unwrap());
+        Self::gen(&n.rhs.clone().unwrap());
+        
+        println!("\tpop rdi");
+        println!("\tpop rax");
+
+        match n.kind {
+            NodeKind::Add => {
+                println!("\tadd rax, rdi");
+            }
+
+            NodeKind::Sub => {
+                println!("\tsub rax, rdi");
+            }
+
+            NodeKind::Mul => {
+                println!("\timul rax, rdi");
+            }
+
+            NodeKind::Div => {
+                println!("\tcqo");
+                println!("\tidiv rdi");
+            }
+
+            _ => {}
+        }
+
+        println!("\tpush rax");
     }
 }
+
 
 
 fn is_digit(chars: &[char]) -> (Result<i64, std::num::ParseIntError>, usize) {
@@ -64,26 +114,104 @@ fn is_digit(chars: &[char]) -> (Result<i64, std::num::ParseIntError>, usize) {
 
 
 const IGNORE: [char; 2] = [' ', '\t',];
+const SYMBOLS: [char; 6] = ['+', '-', '*', '/', '(', ')'];
 
 
-fn tokenize(s: &String) -> Vec<Token> {
+// Syntax
+// expr    = mul ("+" mul | "-" mul)*
+// mul     = primary ("*" primary | "/" primary)*
+// primary = num | "(" expr ")"
+
+
+fn consume(tokens: &mut VecDeque<Token>, s: &str) -> bool {
+    if tokens.len() > 0 && tokens[0].string == s.to_string() {
+        tokens.pop_front();
+        return true;
+    }
+
+    return false;
+}
+
+
+fn expr(tokens: &mut VecDeque<Token>) -> Rc<Node> {
+    let mut node: Rc<Node> = mul(tokens);
+    loop {
+        if consume(tokens, "+") {
+            node = Rc::new(Node {
+                kind: NodeKind::Add,
+                lhs: Some(node),
+                rhs: Some(mul(tokens)),
+            })
+        } else if consume(tokens, "-") {
+            node = Rc::new(Node {
+                kind: NodeKind::Sub,
+                lhs: Some(node),
+                rhs: Some(mul(tokens)),
+            })
+        } else {
+            return node;
+        }
+    }
+}
+
+
+fn mul(tokens: &mut VecDeque<Token>) -> Rc<Node> {
+    let mut node: Rc<Node> = primary(tokens);
+
+    loop {
+        loop {
+            if consume(tokens, "*") {
+                node = Rc::new(Node {
+                    kind: NodeKind::Mul,
+                    lhs: Some(node.clone()),
+                    rhs: Some(primary(tokens)),
+                })
+            } else if consume(tokens, "/") {
+                node = Rc::new(Node {
+                    kind: NodeKind::Div,
+                    lhs: Some(node.clone()),
+                    rhs: Some(primary(tokens)),
+                })
+            } else {
+                return node;
+            }
+        }
+    }
+}
+
+
+fn primary(tokens: &mut VecDeque<Token>) -> Rc<Node> {
+    if consume(tokens, "(") {
+        let node: Rc<Node> = expr(tokens);
+        consume(tokens, ")");
+        return node;
+    }
+
+    return Rc::new(Node {
+        kind: NodeKind::Num { value: tokens.pop_front().unwrap().val.unwrap() },
+        lhs: None,
+        rhs: None,
+    })
+}
+
+
+fn tokenize(s: &String) -> VecDeque<Token> {
     let codes: Vec<char> = s.chars().collect();
     let mut i: usize = 0;
 
-    let mut tokens: Vec<Token> = vec![];
-
+    let mut tokens: VecDeque<Token> = VecDeque::new();
 
     while i < codes.len() {
         if IGNORE.contains(&codes[i]) {
             i += 1;
-        } else if codes[i] == '+' || codes[i] == '-' {
-            tokens.push(
+        } else if SYMBOLS.contains(&codes[i]){
+            tokens.push_back(
                 Token::new(TokenKind::Reserved, None, codes[i].to_string())
             );
             i += 1;
         } else if let (Ok(digit), digit_i) = is_digit(&codes[i..]) {
-            tokens.push(
-                Token::new(TokenKind::Num, Some(digit), codes[i].to_string())
+            tokens.push_back(
+                Token::new(TokenKind::Num, Some(digit), "".to_string())
             );
             i += digit_i;
         } else {
@@ -91,7 +219,6 @@ fn tokenize(s: &String) -> Vec<Token> {
         }
     }
 
-    tokens.push(Token::new(TokenKind::EOF, None, "".to_string()));
     return tokens;
 }
 
@@ -103,41 +230,15 @@ fn main() {
         panic!("Number of arguments invalid");
     }
 
-    let tokens = tokenize(&argv[1]);
+    let mut tokens: VecDeque<Token> = tokenize(&argv[1]);
 
     println!(".intel_syntax noprefix");
     println!(".globl main");
     println!("main:");
 
-    println!("\tmov rax, {}", tokens[0].expect_number());
+    let node: Rc<Node> = expr(&mut tokens); 
+    Node::gen(&node);
 
-    let mut i :usize = 1;
-    while i < tokens.len() {
-        match tokens[i].kind {
-            TokenKind::Reserved => {
-                if tokens[i].string == "+".to_string() {
-                    i += 1;
-                    println!("\tadd rax, {}", tokens[i].expect_number());
-                }
-                if tokens[i].string == "-".to_string() {
-                    i += 1;
-                    println!("\tsub rax, {}", tokens[i].expect_number());
-                }
-            }   
-
-            TokenKind::EOF => {
-                println!("\tret");
-                break;
-            }
-
-            _ => {
-                panic!("Number token doubled");
-            }
-        }
-
-        i += 1;
-    }
-
-
-
+    println!("\tpop rax");
+    println!("\tret");
 }
